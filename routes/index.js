145ff50afa5a988736d3edd32b353af1ef55e0a4
Manu <Manu@KEVINS-LAPTOP>
2020-12-06 00:30:58 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Books collection' });
+});
+
+router.get('/about', function (req, res, next) {
+    res.render('about', { title: ' books colleciton' });
+});
+
+router.get('/login', function (req, res, next) {
+    res.render('login', { title: ' login' });
+});
+
+router.get('/register', function (req, res, next) {
+    res.render('register', { title: ' register' });
+});
+
+module.exports = router;
